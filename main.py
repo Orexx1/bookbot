6690d46235970c7310b2d1333df11b2fb11101d4
Orexx1 <dustin.m117@gmail.com>
2025-02-17 01:22:34 -0700

    updated main.py to new lesson paramter

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
+def charactercount(characters):
+ character_count = {}
+ characters = characters.lower()
+ for character in characters:
+  if character in character_count:
+     character_count[character] += 1
+  else:  
+       character_count[character] = 1
+ return character_count    
+  
+  
+def wordcount(text):
+  return len(text.split())
 def main():
  with open("books/frankenstein.txt") as f:
   file_contents = f.read()
- print(file_contents)
+ count = wordcount(file_contents)
+ print (count)
+ totalcharacters = charactercount(file_contents)
+ print(f"Total unique characters: {len(totalcharacters)}")  # Number of unique characters
+ # Print a sample of character counts
+ print("Sample of character frequencies:")
+ print(dict(list(totalcharacters.items())[:10]))  # Display the first 10 character frequencies
 main()
+
