7266c7ed7edd12c1626efb6395e6f5704d015421
Orexx1 <dustin.m117@gmail.com>
2025-02-18 20:44:30 -0700

    made final changes to bookbot functions for full function

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+def sorting(letters):
+  return letters["count"]
+
 def charactercount(characters):
  character_count = {}
  characters = characters.lower()
@@ -11,15 +14,24 @@ def charactercount(characters):
   
 def wordcount(text):
   return len(text.split())
+
+
 def main():
  with open("books/frankenstein.txt") as f:
   file_contents = f.read()
  count = wordcount(file_contents)
- print (count)
  totalcharacters = charactercount(file_contents)
- print(f"Total unique characters: {len(totalcharacters)}")  # Number of unique characters
- # Print a sample of character counts
- print("Sample of character frequencies:")
- print(dict(list(totalcharacters.items())[:10]))  # Display the first 10 character frequencies
+
+ letters = [{"char": char, "count": char_count} for char, char_count in totalcharacters.items() if char.isalpha()]
+
+ letters.sort(reverse=True, key=sorting)
+  
+ 
+ print(f"--- Begin report of books/frankenstein.txt ---")
+ print(f"{count} words found in the document\n")
+ 
+ for letter in letters:
+    print(f"The '{letter['char']}' character was found {letter['count']} times")
+ print("--- End report ---")
 main()
 
