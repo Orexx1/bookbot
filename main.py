22ce13e3b7a6083f5ccdd2f8bed5b6d35d1ffd25
Orexx1 <dustin.m117@gmail.com>
2025-03-02 10:37:00 -0700

    updated text file and program file

--- main.py
+++ main.py
@@ -1,20 +1,10 @@
+from stats import charactercount
+
 def sorting(letters):
   return letters["count"]
 
-def charactercount(characters):
- character_count = {}
- characters = characters.lower()
- for character in characters:
-  if character in character_count:
-     character_count[character] += 1
-  else:  
-       character_count[character] = 1
- return character_count    
-  
-  
 def wordcount(text):
   return len(text.split())
-
 
 def main():
  with open("books/frankenstein.txt") as f:
---
+++ stats.py
@@ -0,0 +1,10 @@
+def charactercount(characters):
+ character_count = {}
+ characters = characters.lower()
+ for character in characters:
+  if character in character_count:
+     character_count[character] += 1
+  else:  
+       character_count[character] = 1
+ return character_count    
+  
